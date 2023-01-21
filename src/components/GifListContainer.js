587faef26285da4798227fa50dbf45667a78fce8
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-21 14:29:25 +0300

    Completed Assignment

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,17 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import GifList from "./GifList";
 import GifSearch from "./GifSearch";
 
 function GifListContainer() {
   const [gifs, setGifs] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      "https://api.giphy.com/v1/gifs/search?api_key=EGsouQgqTDSKUd33jpRVzc3r078vs8UI&q=&limit=3&offset=0&rating=g&lang=en"
-    )
-      .then((res) => res.json())
-      .then((data) => setGifs(data.data));
-  }, []);
 
   const handleSubmit = (query) => {
     fetch(
